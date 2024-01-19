fe2bb8a00cd0cc050cba94f1d7342e501be50527
YudinDP <146605173+YudinDP@users.noreply.github.com>
2024-01-19 15:38:05 +0900

    Add files via upload

---
+++ PasswordGenerator/PasswordGenerator/Class1.cs
@@ -0,0 +1,53 @@
+﻿using System.IO;
+namespace PasswordGenerator //пространство имен с классом пароля
+{
+    public class ClassPassword  //класс пароля
+    {
+        //поля
+        private string password = ""; //сам пароль
+        private int passwordLength;  //длина пароля
+        private string simbols = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_"; //используемые символы
+        public string path = Directory.GetCurrentDirectory() + "/passwords.txt";
+        public string description;
+
+
+        ///Конструктор
+        public ClassPassword(int passLength) { 
+            passwordLength = passLength;
+        }
+
+
+        ///Генерация пароля и его вывод
+         public string Create_Password(int passwordLength) {
+            int Num;
+            //создание рандомного числа
+            Random rnd = new Random();
+            Restart:
+            for (int i = 0; i < passwordLength; i++) //пока не заполнили всю длину пароля будем геренировать случайные числа из диапазона доступных
+                                                        //и выбирать в пароль символы из заданной строки всех подходящих
+            {
+                Num = rnd.Next(0, simbols.Length - 1);
+                password += simbols[Num];  //записываем символы в строку пароля
+            }
+            if (!(password.Contains("-") || password.Contains("_"))) { //если пароль не содержит хотя бы 1 спецсимвол, то обнуляем его и пробуем создать еще раз
+                password = "";
+                goto Restart; //возвращаемся для повторного заполнения 
+            }
+            return password;  //возвращаем готовый пароль в место вызова
+        }
+
+
+        ///Сохранение пароля в файл
+        public bool SaveInFile() {
+            if (password.Length != passwordLength) { //проверяем все-ли так с нашим паролем
+                return false;  //если нет, то уходим из метода с false
+            }
+            Console.WriteLine("(По желанию) Укажите комментарий к этому паролю, чтобы не забыть от чего он:)");
+            description = Console.ReadLine();  //указываем комменатрий
+            StreamWriter file = new StreamWriter(path, true);  //создаем поток для записи в наш файл(создаем файл) path - путь к файлу(+имя)  true - то, что запись первичная, это почему-то позволяет дозаписывать в файл
+            file.WriteLineAsync(password + " - " + description);  //дописываем в файл данные
+            file.Close();  //закрываем файл
+            return true;  //возвращаемся с успехом
+        }
+    }
+}
---
+++ PasswordGenerator/PasswordGenerator/Program.cs
@@ -0,0 +1,45 @@
+﻿
+using PasswordGenerator;  //используем пространство имен с классом пароля
+Main();
+void Main() {
+    Console.WriteLine("Генератор паролей");
+    Console.WriteLine("Введите желаемую длину пароля(>=8), Если это не важно, то нажмите ENTER:");
+    string passLenStr = Console.ReadLine(); //запрашиваем длину пароля в виде строки и проверяем не пустая ли она
+    int passwordLength;
+    if (passLenStr != "") //если строка не пустая, то преобразуем в число(крайне рассчитываем на здравомыслие пользователя, защиты от НЕ чисел нет)
+                          //todo: защита от НЕ чисел
+    {
+        passwordLength = int.Parse(passLenStr);
+    }
+    else {
+        passwordLength = 10;  //иначе даем стандартную длину пароля
+    }
+    if (passwordLength < 8 || passwordLength > 50) {  //теперь проверяем не слишком ли большое/малое число
+        Console.WriteLine("Длина была не указана или оказалась слишком длинной/короткой. Была выбрана стандартная длина - 10");
+        passwordLength = 10;   //если что-то не так, то уведомляем об этом и даем стандартную длину пароля
+    }
+    ClassPassword Password = new ClassPassword(passwordLength);  //создаем объект класса пароля, передавая в конструктор длину пароля
+    Console.BackgroundColor = ConsoleColor.Blue;
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.WriteLine("Ваш пароль - " + Password.Create_Password(passwordLength)); //выводим на экран готовый пароль, который должны принять большинство систем
+    Console.ResetColor();
+
+    Console.WriteLine("\nРекомендуем опробовать пароль для той системы, где вы хотели его использовать");
+
+    Console.WriteLine("\nСохранить пароль в файл?(крайне рекомендуется убедиться в работоспособности перед этим!)");
+    Console.WriteLine("Да(ENTER)\t\t\tНет(Другая кнопка)");
+    if (Console.ReadKey().Key == ConsoleKey.Enter) //если пользователь соглашается с сохранением, то уходим в меиод сохранения
+    {
+        if (Password.SaveInFile() == true) //если все ок, то выводи сообщение
+        {
+            Console.WriteLine("\nУспешно сохранено в:  папка с программой/passwords.txt");
+        }
+        else {  //если не ок, то тоже
+            Console.WriteLine("\nОшибка при сохранении!");
+        }
+     
+    }
+
+        return; //завершаем программу
+
+}
